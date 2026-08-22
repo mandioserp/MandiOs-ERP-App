56e8b956640cb5dfe2c70dbbfc858428fa25c9cc
malikraheelrugh <raheelrugh677@gmail.com>
2026-08-22 18:21:40 +0500

    updated environment variables

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiBaseUrl = import.meta.env.VITE_API_URL || '/api';
+const apiBaseUrl = import.meta.env.VITE_API_URL || 'https://mandios-erp-app-production.up.railway.app/api';
 
 const api = axios.create({
   baseURL: apiBaseUrl,
